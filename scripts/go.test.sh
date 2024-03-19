070dc2f1ee8b1da541b79f883f4cd267a6b7cb85
Sean Zheng <blackhorseya@gmail.com>
2024-03-19 16:38:45 +0800

    chore: configure Bazel build settings and scripts
    - Add `.bazelrc` file with `--enable_bzlmod` and `--workspace_status_command` settings
    - Add `BUILD.bazel` file with `load` and `gazelle` configurations
    - Add `MODULE.bazel` file with `bazel_dep` and `go_deps` configurations
    - Add `WORKSPACE` file with `workspace` configuration
    - Add `scripts/go.test.sh` file with test script
    - Add `scripts/print-workspace-status.sh` file with script for printing workspace status
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ scripts/go.test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v entity | grep -v docs | grep -v pb | grep -v mock); do
+    go test -race -coverprofile=profile.out -covermode=atomic --tags=unit "$d"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
