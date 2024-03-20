b833cfd248024c1679aa0a3d5dc5dbf42d7a4e82
Sean Zheng <blackhorseya@gmail.com>
2024-03-21 07:27:30 +0800

    feat: consolidate Go library management and define Collection entity
    - Add a new file `BUILD.bazel` for managing Go libraries
    - Add a new file `collection.go` for defining a Collection entity
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/domain/management/model/collection.go
@@ -0,0 +1,8 @@
+package model
+
+// Collection is an entity for word collection.
+type Collection struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	OwnerID string `json:"owner_id"`
+}
