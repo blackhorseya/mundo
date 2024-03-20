53e74e653af7f6cbc9e9bc86fe37b4d683ec23d2
Sean Zheng <blackhorseya@gmail.com>
2024-03-20 12:12:13 +0800

    chore: refactor OpenAIX client package
    - Add `pkg/contextx` as a dependency in `pkg/openaix/BUILD.bazel`
    - Add constants for default values in `pkg/openaix/client.go`
    - Add a new function `CreateFunctionCall` in `pkg/openaix/client.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/openaix/client.go
+++ pkg/openaix/client.go
@@ -2,7 +2,15 @@ package openaix
 
 import (
 	"github.com/blackhorseya/mundo/pkg/configx"
+	"github.com/blackhorseya/mundo/pkg/contextx"
 	"github.com/sashabaranov/go-openai"
+)
+
+const (
+	defaultMaxTokens   = 300
+	defaultModel       = openai.GPT3Dot5Turbo
+	defaultTemperature = 1.0
+	defaultTopP        = 1.0
 )
 
 // Client is the client for openai.
@@ -17,4 +25,28 @@ func NewClient() (*Client, error) {
 	return &Client{
 		Client: client,
 	}, nil
+}
+
+// CreateFunctionCall is used to create a function call.
+func (c *Client) CreateFunctionCall(
+	ctx contextx.Contextx,
+	content string,
+	tools []openai.Tool,
+) (resp openai.ChatCompletionResponse, err error) {
+	return c.Client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model: defaultModel,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: content,
+			},
+		},
+		MaxTokens:        defaultMaxTokens,
+		Temperature:      defaultTemperature,
+		TopP:             defaultTopP,
+		PresencePenalty:  0.0,
+		FrequencyPenalty: 0.0,
+		Tools:            tools,
+		ToolChoice:       "auto",
+	})
 }
