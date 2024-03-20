3755794382fd68e41867d90f093d78c2a552bbc6
Sean Zheng <blackhorseya@gmail.com>
2024-03-20 09:36:35 +0800

    chore: refactor command handling and dependencies in codebase
    - Add dependencies to the BUILD.bazel file
    - Add imports to the cmds.go file
    - Add a new command to the NewCommands function in the cmds.go file
    - Add imports and implement the Execute function in the PingCommand struct in the ping.go file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/cmds/cmds.go
+++ adapter/restful/cmds/cmds.go
@@ -1,10 +1,20 @@
 package cmds
 
+import (
+	"github.com/blackhorseya/mundo/entity/domain/identity/agg"
+	"github.com/blackhorseya/mundo/pkg/contextx"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+)
+
 // TextCommander is the interface for text command.
 type TextCommander interface {
+	// Execute is the function to execute the command.
+	Execute(ctx contextx.Contextx, who *agg.Member, text string) ([]messaging_api.MessageInterface, error)
 }
 
 // NewCommands is the function to create the text commands.
 func NewCommands() []TextCommander {
-	return []TextCommander{}
+	return []TextCommander{
+		&PingCommand{},
+	}
 }
--- adapter/restful/cmds/ping.go
+++ adapter/restful/cmds/ping.go
@@ -1,5 +1,27 @@
 package cmds
 
+import (
+	"github.com/blackhorseya/mundo/entity/domain/identity/agg"
+	"github.com/blackhorseya/mundo/pkg/contextx"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+)
+
 // PingCommand is the command for ping.
 type PingCommand struct {
+}
+
+func (cmd *PingCommand) Execute(
+	ctx contextx.Contextx,
+	who *agg.Member,
+	text string,
+) ([]messaging_api.MessageInterface, error) {
+	if text == "ping" {
+		return []messaging_api.MessageInterface{
+			&messaging_api.TextMessage{
+				Text: "pong",
+			},
+		}, nil
+	}
+
+	return nil, nil
 }
