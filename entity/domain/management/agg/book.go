6013677aae56cedfb5e9a68b3d12e034cc46b1fb
Sean Zheng <blackhorseya@gmail.com>
2024-03-25 00:33:39 +0800

    chore: integrate new dependency and template file
    - Add a dependency to `@com_github_line_line_bot_sdk_go_v8//linebot/messaging_api`
    - Add a new file `entity/domain/management/agg/book.tmpl`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/management/agg/book.go
+++ entity/domain/management/agg/book.go
@@ -2,9 +2,16 @@ package agg
 
 import (
 	"github.com/blackhorseya/mundo/entity/domain/management/model"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 )
 
 // Wordbook is an aggregate for word book.
 type Wordbook struct {
 	model.Collection
+}
+
+// FlexContainer will return the flex container message.
+func (x *Wordbook) FlexContainer() ([]messaging_api.MessageInterface, error) {
+	// todo: 2024/3/25|sean|implement me
+	return nil, nil
 }
