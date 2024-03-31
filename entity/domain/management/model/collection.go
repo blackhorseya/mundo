616d080b9b3efdaec0bee8de7dd7d8e5f22bcc6d
Sean Zheng <blackhorseya@gmail.com>
2024-03-31 17:50:28 +0800

    feat: update data model with new field for words in collection struct
    - Add a new field `Words` of type `[]string` to the `Collection` struct
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/management/model/collection.go
+++ entity/domain/management/model/collection.go
@@ -2,7 +2,8 @@ package model
 
 // Collection is an entity for word collection.
 type Collection struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	OwnerID string `json:"owner_id"`
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	OwnerID string   `json:"owner_id"`
+	Words   []string `json:"words"`
 }
